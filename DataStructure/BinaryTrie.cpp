763c4c2eb58ebe524a5c309bae315e66d9b6dba3
TAB0905 <t.yotaro0905@gmail.com>
2020-06-24 13:57:30 +0900

    add Binary Trie

---
+++ DataStructure/BinaryTrie.cpp
@@ -0,0 +1,16 @@
+#include <array>
+
+template<int length>
+class BinaryTrie {
+private:
+  struct Node {
+    int size;
+    std::array<std::unique_ptr<Node>,2> children;
+  };
+  Node root;
+public:
+  void insert();
+  void erase();
+  long long lower_bound();
+  long long upper_bound();
+};
