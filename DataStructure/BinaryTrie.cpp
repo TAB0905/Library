71308665b3e52a9ebe04654a38bc9393445689f6
TAB0905 <t.yotaro0905@gmail.com>
2020-09-24 03:15:57 +0900

    fix

--- DataStructure/BinaryTrie.cpp
+++ DataStructure/BinaryTrie.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 
 template<typename T, int len>
@@ -34,17 +35,13 @@ public:
     x ^= xor_value;
     if(!cnt) return;
     v->size -= cnt;
-    for(int i = len-1; i >= 0; --i){//root -> leaf
+    for(int i = len-1; i >= 0; --i){
       int b = (x>>i)&1;
-      assert(v->children[b] != nullptr);
       v = v->children[b];
       v->size -= cnt;
     }
-    assert(v->children[0] == nullptr);
-    assert(v->children[1] == nullptr);
-    for(int i = 0; i < len; ++i){//leaf -> root
+    for(int i = 0; i < len; ++i){
       if(v->size) break;
-      assert(v != root);
       auto p = v->parent;
       int b = (x>>i)&1;
       p->children[b] = nullptr;
@@ -74,12 +71,10 @@ public:
           k -= v->children[b]->size;
         b ^= 1;
       }
-      assert(v->children[b] != nullptr);
       ret <<= 1;
       ret |= b;
       v = v->children[b];
     }
-    assert(v->size >= k);
 
     return ret^xor_value;
   }
@@ -102,54 +97,3 @@ public:
     xor_value ^= x;
   }
 };
-
-// ARC033-C
-// https://atcoder.jp/contests/arc033/tasks/arc033_3
-// #include <iostream>
-// using namespace std;
-
-// void ARC033C(){
-//   BinaryTrie<32> bt;
-//   int Q;
-//   cin >> Q;
-//   while(Q--){
-//     int t, x;
-//     cin >> t >> x;
-//     if(t == 1){
-//       bt.insert(x);
-//     }else{
-//       auto y = bt.kth_element(x);
-//       cout << y << "\n";
-//       bt.erase(y);
-//     }
-//   }
-// }
-
-
-// Educational Codeforces Round 12 - E
-// https://codeforces.com/group/zX1lm3q5cU/contest/665/problem/E
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-
-// void edu12E(){
-//   int N, K;
-//   cin >> N >> K;
-
-//   BinaryTrie<int,30> binTrie;
-
-//   vector<int> A(N+1);
-//   for(int i = 0; i < N; ++i){
-//     cin >> A[i+1];
-//     A[i+1] ^= A[i];
-//   }
-
-//   long long ans = 0;
-//   for(int i = 0; i <= N; ++i){
-//     binTrie.xor_all(A[i]);
-//     ans += binTrie.count_leq(K);
-//     binTrie.xor_all(A[i]);
-//     binTrie.insert(A[i]);
-//   }
-//   cout << ans << endl;
-// }
--- test/LibraryChecker/set_xor_min.test.cpp
+++ test/LibraryChecker/set_xor_min.test.cpp
@@ -27,3 +27,55 @@ int main(){
     }
   }
 }
+
+
+// ARC033-C
+// https://atcoder.jp/contests/arc033/tasks/arc033_3
+// #include <iostream>
+// using namespace std;
+
+// void ARC033C(){
+//   BinaryTrie<32> bt;
+//   int Q;
+//   cin >> Q;
+//   while(Q--){
+//     int t, x;
+//     cin >> t >> x;
+//     if(t == 1){
+//       bt.insert(x);
+//     }else{
+//       auto y = bt.kth_element(x);
+//       cout << y << "\n";
+//       bt.erase(y);
+//     }
+//   }
+// }
+
+
+// Educational Codeforces Round 12 - E
+// https://codeforces.com/group/zX1lm3q5cU/contest/665/problem/E
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// void edu12E(){
+//   int N, K;
+//   cin >> N >> K;
+
+//   BinaryTrie<int,30> binTrie;
+
+//   vector<int> A(N+1);
+//   for(int i = 0; i < N; ++i){
+//     cin >> A[i+1];
+//     A[i+1] ^= A[i];
+//   }
+
+//   long long ans = 0;
+//   for(int i = 0; i <= N; ++i){
+//     binTrie.xor_all(A[i]);
+//     ans += binTrie.count_leq(K);
+//     binTrie.xor_all(A[i]);
+//     binTrie.insert(A[i]);
+//   }
+//   cout << ans << endl;
+// }
