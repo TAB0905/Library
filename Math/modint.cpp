d8f00a5b8da49f99a265cc555298cf6f419cb79d
TAB0905 <t.yotaro0905@gmail.com>
2020-12-13 16:53:44 +0900

    update modint

--- Math/modint.cpp
+++ Math/modint.cpp
@@ -1,54 +1,71 @@
 #include <iostream>
+#include <cassert>
 
 template<long long mod>
 class modint{
 private:
-  long long a;
+  using T = long long;
+  T a;
 public:
   constexpr modint(const long long x = 0) noexcept : a((x%mod+mod)%mod) {}
-  constexpr long long& value() noexcept { return a; }
-  constexpr const long long& value() const noexcept { return a; }
-  constexpr modint operator+(const modint rhs) const noexcept {
+  constexpr T& value() noexcept { return a; }
+  constexpr const T& value() const noexcept { return a; }
+  constexpr modint operator-() const noexcept {
+    return modint(0) -= *this;
+  }
+  constexpr modint operator+(const modint& rhs) const noexcept {
     return modint(*this) += rhs;
   }
-  constexpr modint operator-(const modint rhs) const noexcept {
+  constexpr modint operator-(const modint& rhs) const noexcept {
     return modint(*this) -= rhs;
   }
-  constexpr modint operator*(const modint rhs) const noexcept {
+  constexpr modint operator*(const modint& rhs) const noexcept {
     return modint(*this) *= rhs;
   }
-  constexpr modint operator/(const modint rhs) const noexcept {
+  constexpr modint operator/(const modint& rhs) const noexcept {
     return modint(*this) /= rhs;
   }
-  constexpr modint& operator+=(const modint rhs) noexcept {
+  constexpr modint& operator+=(const modint& rhs) noexcept {
     a += rhs.a;
     if(a >= mod) a -= mod;
     return *this;
   }
-  constexpr modint &operator-=(const modint rhs) noexcept {
+  constexpr modint &operator-=(const modint& rhs) noexcept {
     if(a < rhs.a) a += mod;
     a -= rhs.a;
     return *this;
   }
-  constexpr modint& operator*=(const modint rhs) noexcept {
+  constexpr modint& operator*=(const modint& rhs) noexcept {
     a = a*rhs.a%mod;
     return *this;
   }
-  constexpr modint& operator/=(modint rhs) noexcept {
-    long long k = mod-2;
-    while(k > 0){
-      if(k&1){
-        *this *= rhs;
-      }
-      rhs *= rhs;
-      k /= 2;
-    }
-    return *this;
+  constexpr modint& operator/=(const modint& rhs) noexcept {
+    return *this *= rhs.inv();
   }
-  friend std::ostream& operator<<(std::ostream &os, const modint &X){
+  constexpr bool operator==(const modint& rhs) const noexcept {
+    return a == rhs.a;
+  }
+  constexpr bool operator!=(const modint& rhs) const noexcept {
+    return not (*this == rhs);
+  }
+  constexpr modint pow(long long k) const noexcept {
+    modint ret(1);
+    modint x = k > 0 ? *this : this->inv();
+    k = abs(k);
+    while(k > 0){
+      if(k&1) ret *= x;
+      x *= x;
+      k >>= 1;
+    }
+    return ret;
+  }
+  constexpr modint inv() const noexcept {
+    return pow(mod-2);
+  }
+  friend std::ostream& operator<<(std::ostream &os, const modint &X) noexcept {
     return os << X.a;
   }
-  friend std::istream& operator>>(std::istream &is, modint &X){
+  friend std::istream& operator>>(std::istream &is, modint &X) noexcept {
     is >> X.a;
     X.a %= mod;
     if(X.a < 0) X.a += mod;
