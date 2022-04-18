3fa33441b56f7dfccb9d63c70b08c16e02d033f8
TAB0905 <t.yotaro0905@gmail.com>
2022-04-18 22:05:41 +0900

    update

---
+++ Misc/Compressor.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <vector>
+
+template<typename T>
+class Complessor {
+  std::vector<T> m_val;
+public:
+  Complessor(const std::vector<T> dat) : m_val(dat) {
+    std::sort(m_val.begin(),m_val.end());
+    m_val.erase(std::unique(m_val.begin(),m_val.end()),m_val.end());
+  }
+  int idx(T x){
+    return std::lower_bound(m_val.begin(),m_val.end()) - m_val.begin();
+  }
+  int val(int i){
+    return m_val[i];
+  }
+};
+
